63b2876335be7b80e8c176d80b1368a1d8a937b5
efforthye <efforthye@gmail.com>
2024-04-21 01:56:46 +0900

    feat: language UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.scss';
-import { useState, ChangeEvent, useEffect } from 'react';
+import { useState, ChangeEvent, useEffect, useRef } from 'react';
 import { BsQuestionSquareFill } from 'react-icons/bs';
 import { BsFillChatLeftTextFill } from 'react-icons/bs';
 import axios from 'axios';
 import { setDefaultResultOrder } from 'dns';
+
+const serverUrl = "http://13.124.93.106:8080/api";
 
 const CursorSVG = () => {
   return (
@@ -22,9 +24,16 @@ const App = () => {
   const [displayResponse, setDisplayResponse] = useState(['']);
   const [completedTyping, setCompletedTyping] = useState(true);
   const [displayTextArray, setDisplayTextArray] = useState<string[]>([]);
+  const [showTranslate, setShowTranslate] = useState(false);
+  const [selectedTranslate, setSelectedTranslate] = useState("ko");
 
   // input 값이 변경될 때마다 호출되는 함수
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => setInputValue(event.target.value); // 입력값을 state에 반영합니다.
+
+  const openTranslate = () => {
+    setShowTranslate(!showTranslate); // showTranslate 상태를 토글
+  };
+  
 
   const onClick = async () => {
     setQuestion(inputValue);
@@ -32,7 +41,7 @@ const App = () => {
     setChatHistory([...chatHistory, { user: 'AI', content: [`${title}가 답변중입니다...`] }]);
 
     // 영문 답변
-    const englishResult = await axios.get('http://13.124.93.106:8080/api/test', {
+    const englishResult = await axios.get(`${serverUrl}/test`, {
       params: {
         text: inputValue,
       },
@@ -43,7 +52,7 @@ const App = () => {
 
     // 한글 답변
     const koreanRes = (
-      await axios.get('http://13.124.93.106:8080/api/translate', {
+      await axios.get(`${serverUrl}/translate`, {
         params: {
           text: englishResData,
         },
@@ -57,14 +66,8 @@ const App = () => {
     }).join('\n');
 
     const splitedEnglishData = englishResData.split("```");
-    const splitedKoreanData = koreanData.split("'''");
 
-    // setDisplayTextArray(splitedKoreanData)
-    // console.log({splitedKoreanData});
-
-    console.log({splitedEnglishData});
-
-    const resultText = `안녕하세요.\n표준프레임워크센터 AI, ${title} 입니다.\n\n${koreanData}\n\n감사합니다.`;
+    const resultText = `안녕하세요.\n표준프레임워크센터 AI ${title} 입니다.\n\n${koreanData}\n\n감사합니다.`;
     const splitedText = resultText.split("'''").map((text, index)=>{
       
       const isCode = index%2 == 1;
@@ -81,16 +84,14 @@ const App = () => {
   useEffect(() => {
     const runTypingAnimation = async () => {
       const originContent = chatHistory[1]?.content;
-      console.log({originContent});
       if (!originContent) return;
-      // animation start
+
+      // 텍스트 생성 애니메이션 시작
       setCompletedTyping(false);
-      // displayResponse string 배열이 화면에 띄워지게 됨
-      console.log({originContent : originContent[originContent.length - 1]})
 
       for(let i = 0; i<originContent.length; ++i) {
+        // 각 텍스트 그룹이 생성될 때까지 기다림
         await new Promise((resolve) => {
-
           let j = 0;
           const intervalId = setInterval(() => {
             const tempString = originContent[i].slice(0, j);
@@ -108,75 +109,92 @@ const App = () => {
         })
       }
 
-
-      // animation fin
-      setCompletedTyping(true); // 모든 애니메이션이 끝난 후에 상태 업데이트
+      // 모든 애니메이션이 끝난 후 상태 업데이트
+      setCompletedTyping(true); 
     };
   
     runTypingAnimation();
   }, [chatHistory]); // chatHistory 변경 시에만 useEffect 실행
   
 
-  return (
-    <div className={styles.container}>
-      {/* <div className={styles.header}>
-        <div></div>
-        <div>
-          <img className={styles.translate} src='translate.png' alt='번역'></img>
+  return (<>
+    <div className={styles.header}>
+          <div></div>
+          <div className={styles.translate} onClick={()=>{
+            openTranslate();
+          }}>
+            
+            <img onClick={() => openTranslate()} className={styles.translateImg} src='translate.png' alt='번역'></img>
+           {showTranslate ?  <ul className={showTranslate ? styles.open : ""} >
+              <li onClick={(e) => {setSelectedTranslate("ko")}}>KO(Korean) - 한국어</li>
+              <li onClick={(e) => {setSelectedTranslate("en")}}>EN(English) - 영어</li>
+              <li onClick={(e) => {setSelectedTranslate("vi")}}>VI(Vietnamese) - 베트남어</li>
+              <li onClick={(e) => {setSelectedTranslate("mn")}}>MN(Mongolian) - 몽골어</li>
+              <li onClick={(e) => {setSelectedTranslate("ne")}}>NE(Nepali) - 네팔어</li>
+              <li onClick={(e) => {setSelectedTranslate("ar-sa")}}>AR-SA(Arabic) - 사우디아라비아어</li>
+              <li onClick={(e) => {setSelectedTranslate("es-mx")}}>ES-MX(Spanish) - 멕시코어</li>
+              <li onClick={(e) => {setSelectedTranslate("bg")}}>BG(Bulgarian) - 불가리아어</li>
+              <li onClick={(e) => {setSelectedTranslate("es-ec")}}>ES-EC(Spanish) - 에콰도르어</li>
+              <li onClick={(e) => {setSelectedTranslate("ar-tn")}}>AR-TN(Arabic) - 튀니지어</li>
+              <li onClick={(e) => {setSelectedTranslate("sw")}}>SW(Swahili) - 탄자니아어</li>
+            </ul> : <></>}
+          <div className={styles.selected}>{selectedTranslate.toUpperCase()}</div>
+          </div>
         </div>
-      </div> */}
-      <img className={styles.titleImage} src='small_title.png' alt='타이틀 이미지'></img>
-      {/* 큰 페이지 제목 */}
-      {/* <h1 className={styles.pageTitle}>{title}</h1> */}
+    <div className={styles.container}>
+    <img className={styles.titleImage} src='small_title.png' alt='타이틀 이미지'></img>
+    {/* 큰 페이지 제목 */}
+    {/* <h1 className={styles.pageTitle}>{title}</h1> */}
 
-      <div className={styles.containerBox}>
-        {/* 나머지 입력 요소들 */}
-        <br />
-        <br />
-        <div className={styles.inputContainer}>
-          {/* 메인 */}
-          <div className={styles.title}>전자 정부 표준 프레임워크 Search AI</div>
-          <div className={styles.article}>
-            <div className={styles.questionbox}>
-              <div className={styles.icon}>
-                <BsQuestionSquareFill size={40} color='orange' className={styles.icon} />
-              </div>
-              <input
-                type='text'
-                value={inputValue}
-                onChange={handleChange}
-                placeholder='여기에 입력하세요.'
-                className={styles.inputField}
-              />
+    <div className={styles.containerBox}>
+      {/* 나머지 입력 요소들 */}
+      <br />
+      <br />
+      <div className={styles.inputContainer}>
+        {/* 메인 */}
+        <div className={styles.title}>전자 정부 표준 프레임워크 Search AI</div>
+        <div className={styles.article}>
+          <div className={styles.questionbox}>
+            <div className={styles.icon}>
+              <BsQuestionSquareFill size={40} color='orange' className={styles.icon} />
+            </div>
+            <input
+              type='text'
+              value={inputValue}
+              onChange={handleChange}
+              placeholder='여기에 입력하세요.'
+              className={styles.inputField}
+            />
 
-              <button className={styles.submit} onClick={onClick} disabled={!inputValue || !completedTyping}>
-                검색
-              </button>
+            <button className={styles.submit} onClick={onClick} disabled={!inputValue || !completedTyping}>
+              검색
+            </button>
+          </div>
+          <div className={styles.yourQuestionContainer}>
+            <div className={styles.icon} />
+            <div className={styles.yourQuestion}>{question}</div>
+          </div>
+          <hr className={styles.line} />
+          <div className={styles.answer}>
+            <div className={styles.icon}>
+              <BsFillChatLeftTextFill size={40} color='green' />
             </div>
-            <div className={styles.yourQuestionContainer}>
-              <div className={styles.icon} />
-              <div className={styles.yourQuestion}>{question}</div>
-            </div>
-            <hr className={styles.line} />
-            <div className={styles.answer}>
-              <div className={styles.icon}>
-                <BsFillChatLeftTextFill size={40} color='green' />
-              </div>
-              <span className={styles.displayResponse}>
-                {/* {displayResponse} */}
-                {displayResponse.map((data, index) => {
-                  const predicate = index % 2 === 0;
-                  const returnTag = <pre className={predicate ? "" : styles.code} key={data + index}>{data.trim()}</pre>;
-                  return returnTag;
-                })}
-                {!completedTyping && <CursorSVG />}
-              </span>
-            </div>
+            <span className={styles.displayResponse}>
+              {/* {displayResponse} */}
+              {displayResponse.map((data, index) => {
+                const predicate = index % 2 === 0;
+                const returnTag = <pre className={predicate ? "" : styles.code} key={data + index}>{data.trim()}</pre>;
+                return returnTag;
+              })}
+              {!completedTyping && <CursorSVG />}
+            </span>
           </div>
         </div>
       </div>
-      <div className={styles.footer}></div>
     </div>
+    <div className={styles.footer}></div>
+    </div>
+  </>
   );
 };
 
